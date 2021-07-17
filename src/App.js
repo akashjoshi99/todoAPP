b68134c1a2f526dfbcbba870943e718557f15016
akashjoshi99 <akashmohan.joshi2019@vitstudent.ac.in>
2021-07-17 12:47:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+import "./App.css"
+
+function App() {
+    return (
+        <div className="todoApp">
+            <TodoList />
+        </div>
+    )
+}
+
+export default App
